58ecc8b54a97315553c6c12a4f4aa5d3c79d2557
Willyan Costa <willyancr@gmail.com>
2024-03-22 15:38:43 -0300

    feat: Remove Logo import and update menu styles

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Logo from '../image/Logo';
 import {
   BiCog,
   BiHomeAlt,
@@ -12,35 +11,35 @@ import {
 
 function Menu() {
   return (
-    <div className="bg-projeto-primary w-52 p-8 rounded-xl text-projeto-primary3">
+    <div className="bg-menu text-projeto-primary3 w-52 rounded-xl rounded-r-lg h-screen ">
       {/* <Logo /> */}
-      <div className="flex gap-2 items-center mb-14">
+      <div className="flex gap-2 items-center px-7 pt-7 mb-14">
         <BiSolidBank className="text-projeto-secondary1 size-7" />
-        <h1 className="text-projeto-secondary font-bold">FINTECH</h1>
+        <h1 className="text-projeto-secondary font-bold text-xl">FINTECH</h1>
       </div>
-      <div>
-        <div className="flex gap-3  mb-6">
-          <BiHomeAlt className="text-projeto-primary3 size-5" />
+      <div className="px-5">
+        <div className="flex gap-3 mb-6 px-2 py-1 rounded hover:bg-projeto-secondary/50 hover:text-projeto-primary hover:font-bold hover:cursor-pointer ease-in duration-150">
+          <BiHomeAlt className=" size-5 " />
           <p>Resumo</p>
         </div>
-        <div className="flex gap-3 mb-6">
-          <BiPurchaseTag className="text-projeto-primary3 size-5" />
+        <div className="flex gap-3 mb-6 px-2 py-1 rounded hover:bg-projeto-secondary/50 hover:text-projeto-primary hover:font-bold hover:cursor-pointer ease-in duration-150">
+          <BiPurchaseTag className="size-5" />
           <p>Vendas</p>
         </div>
-        <div className="flex gap-3 mb-6">
-          <BiTransfer className="text-projeto-primary3 size-5" />
+        <div className="flex gap-3 mb-6 px-2 py-1 rounded hover:bg-projeto-secondary/50 hover:text-projeto-primary hover:font-bold hover:cursor-pointer ease-in duration-150">
+          <BiTransfer className="size-5" />
           <p>Webhooks</p>
         </div>
-        <div className="flex gap-3 mb-6">
-          <BiCog className="text-projeto-primary3 size-5" />
+        <div className="flex gap-3 mb-6 px-2 py-1 rounded hover:bg-projeto-secondary/50 hover:text-projeto-primary hover:font-bold hover:cursor-pointer ease-in duration-150">
+          <BiCog className="size-5" />
           <p>Configurações</p>
         </div>
-        <div className="flex gap-3 mb-6">
-          <BiEnvelope className="text-projeto-primary3 size-5" />
+        <div className="flex gap-3 mb-6 px-2 py-1 rounded hover:bg-projeto-secondary/50 hover:text-projeto-primary hover:font-bold hover:cursor-pointer ease-in duration-150">
+          <BiEnvelope className="size-5" />
           <p>Contato</p>
         </div>
-        <div className="flex gap-3 mb-6">
-          <BiExit className="text-projeto-primary3 size-5" />
+        <div className="flex gap-3 mb-6 px-2 py-1 rounded hover:bg-projeto-secondary/50 hover:text-projeto-primary hover:font-bold hover:cursor-pointer ease-in duration-150">
+          <BiExit className="size-5" />
           <p>Sair</p>
         </div>
       </div>
