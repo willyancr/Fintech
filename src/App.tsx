6333fbe03e13c74e751bfadd8e13e289992a0bc3
Willyan Costa <willyancr@gmail.com>
2024-03-21 22:32:46 -0300

    feat: Add React Icons and update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import React from 'react';
+import Logo from './image/Logo';
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Logo />
     </div>
   );
 }
---
+++ src/image/Logo.tsx
@@ -0,0 +1,9 @@
+function Logo() {
+  return (
+    <div>
+      <img src="./src/image/1_LOGOMARCA_FINTECH.png" alt="" className="w-40" />
+    </div>
+  );
+}
+
+export default Logo;
