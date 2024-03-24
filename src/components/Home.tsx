56240f77611a0b00f55102c19082b797b0ce6e7e
Willyan Costa <willyancr@gmail.com>
2024-03-23 23:09:49 -0300

    style: Update Home component layout styling
    Add justify-center class to center content horizontally in the Home component.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ import Content from '../components/Content';
 
 function Home() {
   return (
-    <div className="flex m-12 h-auto drop-shadow-2xl">
+    <div className="flex m-12 h-auto drop-shadow-2xl justify-center">
       <Menu />
       <Content />
     </div>
