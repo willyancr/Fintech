9755b89505cfee49dff052f590a64c24d5c8539f
Willyan Costa <willyancr@gmail.com>
2024-03-23 22:07:25 -0300

    feat: Add Home component with Menu and Content subcomponents
    Create a new Home component with Menu and Content subcomponents for displaying
    a layout with a menu and content section.

---
+++ src/components/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from '../components/Menu';
+import Content from '../components/Content';
+
+function Home() {
+  return (
+    <div className="flex m-12 h-auto drop-shadow-2xl">
+      <Menu />
+      <Content />
+    </div>
+  );
+}
+
+export default Home;
