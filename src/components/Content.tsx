0efafb98192c92646ccfaf012043ff249763bcd5
Willyan Costa <willyancr@gmail.com>
2024-03-23 23:24:38 -0300

    feat: Add Content component for dashboard
    Create a new Content component for the dashboard with necessary styles and elements.

---
+++ src/components/Content.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Card, CardContent, CardDescription } from './ui/card';
+
+function Content() {
+  return (
+    <div className="bg-projeto-primary rounded-xl rounded-l-lg px-8 py-7">
+      <h1 className="text-projeto-primary3 text-xl font-bold mb-4">
+        Dashboard
+      </h1>
+      <div className="text-projeto-primary3 mb-4">
+        <div className="flex gap-4">
+          <Card className="rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-80">
+            <div className="flex gap-6 pt-4 pb-2 px-4">
+              <p className="flex items-center bg-projeto-primary1 text-projeto-primary3 rounded w-32 pl-2 py-1 font-bold text-sm">
+                Início
+              </p>
+              <p className="flex items-center bg-projeto-primary1 text-projeto-primary3 rounded w-32 pl-2 py-1 font-bold text-sm">
+                Final
+              </p>
+            </div>
+            <div className="flex gap-6 pb-4 px-4">
+              <input
+                type="date"
+                className="w-32 bg-projeto-primary1 text-projeto-primary3 rounded text-sm pl-2 py-1"
+              />
+              <input
+                type="date"
+                className="w-32 bg-projeto-primary1 text-projeto-primary3 rounded text-sm pl-2 py-1"
+              />
+            </div>
+          </Card>
+          <Card className="rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-80">
+            <h1 className="pt-4 pb-2 px-4 text-2xl font-bold">Resumo</h1>
+          </Card>
+        </div>
+      </div>
+      <div className="flex justify-between gap-2 text-projeto-primary3">
+        <p className="font-bold rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-36 py-1 text-center hover:bg-projeto-secondary/80 hover:text-projeto-primary hover:cursor-pointer ease-in duration-150">
+          Janeiro
+        </p>
+        <p className="font-bold rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-36 py-1 text-center hover:bg-projeto-secondary/80 hover:text-projeto-primary hover:cursor-pointer ease-in duration-150">
+          Janeiro
+        </p>
+        <p className="font-bold rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-36 py-1 text-center hover:bg-projeto-secondary/80 hover:text-projeto-primary hover:cursor-pointer ease-in duration-150">
+          Janeiro
+        </p>
+        <p className="font-bold rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-36 py-1 text-center hover:bg-projeto-secondary/80 hover:text-projeto-primary hover:cursor-pointer ease-in duration-150">
+          Janeiro
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Content;
