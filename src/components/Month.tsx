c9f40d1189d07d49950d6d4c09a96887c5a73121
Willyan Costa <willyancr@gmail.com>
2024-03-24 11:25:12 -0300

    feat: Add Month component for displaying future months
    Create a new Month component to display the names of the next 4 months in Portuguese. This component uses React hooks to manage state and effect for updating the months dynamically.

---
+++ src/components/Month.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Month() {
+  const [months, setMonths] = React.useState<{ month: string }[]>([]);
+
+  React.useEffect(() => {
+    const futureMonth = [];
+    for (let i = 1; i <= 4; i++) {
+      const date = new Date();
+      date.setMonth(date.getMonth() + i);
+
+      futureMonth.push({
+        month: date.toLocaleDateString('pt-BR', { month: 'long' }),
+      });
+
+      setMonths(futureMonth);
+    }
+  }, []);
+
+  return (
+    <div className="flex justify-between gap-2 text-projeto-primary3">
+      {months.map((month) => (
+        <p className="font-bold rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-36 py-1 text-center hover:bg-projeto-secondary/80 hover:text-projeto-primary hover:cursor-pointer ease-in duration-150 capitalize">
+          {month.month}
+        </p>
+      ))}
+    </div>
+  );
+}
+
+export default Month;
