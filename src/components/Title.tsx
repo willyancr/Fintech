d833ed57e3fd5667a3ead963d1fae7906cc2116c
Willyan Costa <willyancr@gmail.com>
2024-03-25 14:38:47 -0300

    Refactor Title component to accept children prop
    Update Title component to accept children prop for dynamic content rendering.

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Card } from './ui/card';
 
-function Title() {
+type Props = {
+  children: string;
+};
+
+function Title({ children }: Props) {
   return (
-    <Card className="rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-80">
-      <h1 className="pt-4 pb-2 px-4 text-2xl font-bold">Resumo</h1>
+    <Card className="rounded-[6px] bg-projeto-primary1/50 border-[1px] border-projeto-primary1/20 w-80">
+      <h1 className="pt-4 pb-2 px-4 text-2xl font-bold">{children}</h1>
     </Card>
   );
 }
