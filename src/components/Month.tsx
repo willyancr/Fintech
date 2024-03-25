c2dc20043889d4700724c2eba8790eae0e901423
Willyan Costa <willyancr@gmail.com>
2024-03-25 13:47:37 -0300

    style: Update Month component styles for better readability
    Adjust padding in month component for improved visual appearance and readability.

--- src/components/Month.tsx
+++ src/components/Month.tsx
@@ -5,7 +5,7 @@ function Month() {
 
   React.useEffect(() => {
     const futureMonth = [];
-    for (let i = 1; i <= 4; i++) {
+    for (let i = 0; i < 4; i++) {
       const date = new Date();
       date.setMonth(date.getMonth() + i);
 
@@ -20,7 +20,7 @@ function Month() {
   return (
     <div className="flex justify-between gap-2 text-projeto-primary3">
       {months.map((month) => (
-        <p className="font-bold rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-36 py-1 text-center hover:bg-projeto-secondary/80 hover:text-projeto-primary hover:cursor-pointer ease-in duration-150 capitalize">
+        <p className="font-bold rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-36 py-2 text-center hover:bg-projeto-secondary/80 hover:text-projeto-primary hover:cursor-pointer ease-in duration-150 capitalize">
           {month.month}
         </p>
       ))}
