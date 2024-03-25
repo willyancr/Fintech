124d00f56205272a31c90bfe0dc65dfb807ff8dd
Willyan Costa <willyancr@gmail.com>
2024-03-25 13:46:53 -0300

    feat: Add Sales component to display sales information
    Create a new Sales component to display sales data with different categories
    such as total sales, amount received, and amount processing. This component
    uses Tailwind CSS classes for styling.

---
+++ src/components/Sales.tsx
@@ -0,0 +1,20 @@
+function Sales() {
+  return (
+    <div className="flex justify-between gap-2 text-projeto-primary3">
+      <div className="flex flex-col gap-4 font-bold rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-52 pl-4 py-2">
+        <p>Vendas</p>
+        <p>R$ 84.200,00</p>
+      </div>
+      <div className="flex flex-col gap-4 font-bold rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-52 pl-4 py-2">
+        <p>Recebido</p>
+        <p>R$ 54.200,00</p>
+      </div>
+      <div className="flex flex-col gap-4 font-bold rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-52 pl-4 py-2">
+        <p>Processando</p>
+        <p>R$ 34.200,00</p>
+      </div>
+    </div>
+  );
+}
+
+export default Sales;
