8ea309ac39b97ee413ff25d6a2a43b5eb6979174
Willyan Costa <willyancr@gmail.com>
2024-03-25 14:40:33 -0300

    Add GraphicResume component
    Create GraphicResume component with basic structure.

---
+++ src/components/GraphicResume.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GraphicResume() {
+  return (
+    <div className="text-projeto-primary3 bg-projeto-primary1/10 h-80 p-10 rounded-[6px] border-[1px] border-projeto-primary1/20">
+      GraphicResume
+    </div>
+  );
+}
+
+export default GraphicResume;
