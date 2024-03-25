37f3431591c07189f3133e7ef1c6533cd297ed36
Willyan Costa <willyancr@gmail.com>
2024-03-25 14:40:24 -0300

    Update component imports and routing in Home component
    Import Resume, Sales, and Menu components. Update routing using BrowserRouter,
    Routes, and Route components for different paths.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,18 @@
-import Content from '../components/Content';
+import Resume from './Resume';
+import Sales from './Sales';
+import Menu from '../components/Menu';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function Home() {
   return (
-    <div>
-      <Content />
+    <div className="flex m-12 h-auto drop-shadow-2xl justify-center">
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<Resume />} />
+          <Route path="/vendas" element={<Sales />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
