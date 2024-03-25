3a48c869a613c31ec6fcbf4e68dabf3389937f10
Willyan Costa <willyancr@gmail.com>
2024-03-25 14:40:09 -0300

    Add routing links to Menu component
    Imported Link from react-router-dom and added routing links to Resumo and Vendas
    sections in the Menu component for navigation purposes.

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,26 +7,28 @@ import {
   BiEnvelope,
   BiTransfer,
 } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
 
 function Menu() {
   return (
     <div className="bg-projeto-primary text-projeto-primary3 min-w-52 rounded-xl rounded-r-lg border-r-[1px] border-projeto-primary1/30">
-      {/* <Logo /> */}
-      <div className="flex gap-2 items-center px-7 pt-7 mb-14">
-        <BiSolidBank className="text-projeto-secondary1 size-7" />
-        <h1 className="text-projeto-secondary font-bold text-xl">FINTECH</h1>
+      <div className="flex items-center gap-2 px-7 pt-7 mb-14 text-projeto-secondary1">
+        <BiSolidBank className="size-7" />
+        <h1 className="font-bold text-xl">FINTECH</h1>
       </div>
       <div className="px-5">
-        <div className="flex items-center gap-3 mb-6 px-2 py-1 rounded hover:bg-projeto-secondary/80 hover:text-projeto-primary hover:font-bold hover:cursor-pointer ease-in duration-150">
-          <BiHomeAlt className=" size-5 " />
-          <p>Resumo</p>
-        </div>
-
-        <div className="flex items-center gap-3 mb-6 px-2 py-1 rounded hover:bg-projeto-secondary/80 hover:text-projeto-primary hover:font-bold hover:cursor-pointer ease-in duration-150">
-          <BiPurchaseTag className="size-5" />
-          <p>Vendas</p>
-        </div>
-
+        <Link to="/">
+          <div className="flex items-center gap-3 mb-6 px-2 py-1 rounded hover:bg-projeto-secondary/80 hover:text-projeto-primary hover:font-bold hover:cursor-pointer ease-in duration-150">
+            <BiHomeAlt className=" size-5 " />
+            <p>Resumo</p>
+          </div>
+        </Link>
+        <Link to="/vendas">
+          <div className="flex items-center gap-3 mb-6 px-2 py-1 rounded hover:bg-projeto-secondary/80 hover:text-projeto-primary hover:font-bold hover:cursor-pointer ease-in duration-150">
+            <BiPurchaseTag className="size-5" />
+            <p>Vendas</p>
+          </div>
+        </Link>
         <div className="flex items-center gap-3 mb-6 px-2 py-1 rounded hover:bg-projeto-secondary/80 hover:text-projeto-primary hover:font-bold hover:cursor-pointer ease-in duration-150">
           <BiTransfer className="size-5" />
           <p>Webhooks</p>
