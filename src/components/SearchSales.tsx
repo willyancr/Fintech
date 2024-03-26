526ac2eec544b01940d99705620cdf16c33734f0
Willyan Costa <willyancr@gmail.com>
2024-03-25 22:51:20 -0300

    feat: Refactor SearchSales component to accept search state and setSearch function
    Update SearchSales component to accept search state and setSearch function as props,
    improving reusability and maintainability.

--- src/components/SearchSales.tsx
+++ src/components/SearchSales.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { BiSearch } from 'react-icons/bi';
-function SearchSales() {
+function SearchSales({
+  search,
+  setSearch,
+}: {
+  search: string;
+  setSearch: (value: string) => void;
+}) {
   return (
     <div className="relative">
       <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
         type="text"
         placeholder="Pesquisar pelo nome..."
         className="bg-projeto-primary1/20 text-projeto-primary3 rounded text-sm pl-8 py-2 w-full outline-none"
