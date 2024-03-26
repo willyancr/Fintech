c982ede8f687e9bad40ce51d7921e09bde1f38f2
Willyan Costa <willyancr@gmail.com>
2024-03-26 00:32:46 -0300

    feat: Refactor TriCardStatus component to dynamically display sales data
    Imported useSale hook and updated component to calculate and display total sales,
    received amount, and processing amount dynamically based on data.

--- src/components/TriCardStatus.tsx
+++ src/components/TriCardStatus.tsx
@@ -1,17 +1,37 @@
+import { useSale } from '../components/UserContext';
+
 function TriCardStatus() {
+  const { data, calculateTotalSales, calculateReceived, calculateProcessing } =
+    useSale();
+
   return (
     <div className="flex justify-between gap-2 text-projeto-primary3">
       <div className="flex flex-col gap-4 font-bold rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-52 pl-4 py-2">
         <p>Vendas</p>
-        <p>R$ 84.200,00</p>
+        <p>
+          {calculateTotalSales(data).toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          })}
+        </p>
       </div>
       <div className="flex flex-col gap-4 font-bold rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-52 pl-4 py-2">
         <p>Recebido</p>
-        <p>R$ 54.200,00</p>
+        <p>
+          {calculateReceived(data).toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          })}
+        </p>
       </div>
       <div className="flex flex-col gap-4 font-bold rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-52 pl-4 py-2">
         <p>Processando</p>
-        <p>R$ 34.200,00</p>
+        <p>
+          {calculateProcessing(data).toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          })}
+        </p>
       </div>
     </div>
   );
