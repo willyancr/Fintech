f339ccea78df58073d2523d48f55a1dd92060ef2
Willyan Costa <willyancr@gmail.com>
2024-03-25 22:21:36 -0300

    feat: Add UserContextProvider to Home component
    Wrap existing components in UserContextProvider for user context handling.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,17 +3,20 @@ import Sales from './Sales';
 import Menu from '../components/Menu';
 import SaleDetails from '../components/SaleDetails';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { UserContextProvider } from '../components/UserContext';
 
 function Home() {
   return (
     <div className="flex m-12 h-auto drop-shadow-2xl justify-center">
       <BrowserRouter>
-        <Menu />
-        <Routes>
-          <Route path="/" element={<Resume />} />
-          <Route path="/vendas" element={<Sales />} />
-          <Route path="/vendas/id" element={<SaleDetails />} />
-        </Routes>
+        <UserContextProvider>
+          <Menu />
+          <Routes>
+            <Route path="/" element={<Resume />} />
+            <Route path="/vendas" element={<Sales />} />
+            <Route path="/vendas/id" element={<SaleDetails />} />
+          </Routes>
+        </UserContextProvider>
       </BrowserRouter>
     </div>
   );
