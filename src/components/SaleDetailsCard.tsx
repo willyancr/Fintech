2daa19bbc12eb9223a444ebb8236f7d1add519de
Willyan Costa <willyancr@gmail.com>
2024-03-26 14:37:42 -0300

    feat: Refactor SaleDetailsCard component for better readability
    Update SaleDetailsCard component to use 'selected' instead of 'data' for clarity
    and improve the structure and styling of the displayed sale details.

--- src/components/SaleDetailsCard.tsx
+++ src/components/SaleDetailsCard.tsx
@@ -2,18 +2,23 @@ import { Card } from './ui/card';
 import { useSale } from './UserContext';
 
 function SaleDetailsCard() {
-  const { data } = useSale();
+  const { selected } = useSale();
   return (
     <div>
-      <Card className="rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-80">
-        <div className="flex flex-col gap-6 py-4 px-4 text-projeto-primary3">
-          <p>ID:</p>
-          <p>Nome:</p>
-          <p>Preço:</p>
-          <p>Status:</p>
-          <p>Pagamento:</p>
-        </div>
-      </Card>
+      {selected && (
+        <Card
+          key={selected.id}
+          className="rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-full"
+        >
+          <div className="flex flex-col gap-6 py-4 px-4 text-projeto-primary3 capitalize">
+            <p >ID: <span className='bg-projeto-primary1/50 rounded p-1'>{selected.id}</span> </p>
+            <p>Nome: <span className='bg-projeto-primary1/50 rounded p-1'>{selected.nome}</span> </p>
+            <p>Preço: <span className='bg-projeto-primary1/50 rounded p-1'>R$ {selected.preco}</span> </p>
+            <p>Status: <span className='bg-projeto-primary1/50 rounded p-1'>{selected.status}</span> </p>
+            <p>Pagamento: <span className='bg-projeto-primary1/50 rounded p-1'>{selected.pagamento}</span> </p>
+          </div>
+        </Card>
+      )}
     </div>
   );
 }
