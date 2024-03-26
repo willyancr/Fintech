7a9399fbc64053dada01321b82c0e03b5fb14587
Willyan Costa <willyancr@gmail.com>
2024-03-26 00:41:57 -0300

    Update styles for GraphicResume and SearchSales components
    Adjust the height and padding for GraphicResume component and update
    styles for SearchSales component to improve layout and appearance.

--- src/components/SearchSales.tsx
+++ src/components/SearchSales.tsx
@@ -14,9 +14,9 @@ function SearchSales({
         onChange={(e) => setSearch(e.target.value)}
         type="text"
         placeholder="Pesquisar pelo nome..."
-        className="bg-projeto-primary1/20 text-projeto-primary3 rounded text-sm pl-8 py-2 w-full outline-none"
+        className="bg-projeto-primary1/20 text-projeto-primary3 rounded pl-9 py-4 w-full outline-none"
       />
-      <BiSearch className="text-projeto-primary3 absolute top-[0.7rem] left-2 " />
+      <BiSearch className="text-projeto-primary3 absolute top-4 left-2 size-5" />
     </div>
   );
 }
