bf8a5bcae402336ff8fbbcc2a9e35047dfa73d9b
Willyan Costa <willyancr@gmail.com>
2024-03-25 22:20:46 -0300

    feat: Refactor UserContext component and useSale hook
    Update useSale hook to handle null context and refactor UserContextProvider
    component for better readability and consistency.

--- src/components/UserContext.tsx
+++ src/components/UserContext.tsx
@@ -15,18 +15,21 @@ type UserContextValue = {
   error: null | string;
 };
 const UserContext = React.createContext<UserContextValue | null>(null);
-export const useSale = () => React.useContext(UserContext);
+export const useSale = () => {
+  const context = React.useContext(UserContext);
+  if (context == null) {
+    throw new Error('useSale must be used within a UserContextProvider');
+  }
+  return context;
+};
 
-function UserContextProvider({ children }: React.PropsWithChildren) {
+export const UserContextProvider = ({ children }: React.PropsWithChildren) => {
   const { data, loading, error } = useFetch<Venda>(
-    'https://data.origamid.com.br/vendas',
+    'https://data.origamid.dev/vendas',
   );
-
   return (
     <UserContext.Provider value={{ data, loading, error }}>
       {children}
     </UserContext.Provider>
   );
-}
-
-export default UserContextProvider;
+};
