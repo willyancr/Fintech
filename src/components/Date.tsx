f966ba5bfb4ce7525fe4b689bac5a28bbd68a310
Willyan Costa <willyancr@gmail.com>
2024-03-26 19:39:22 -0300

    feat: Add useSale hook to Date component
    Import and use the useSale hook in the Date component to manage sale dates. Set
    the start and end dates based on user input.

--- src/components/Date.tsx
+++ src/components/Date.tsx
@@ -1,6 +1,8 @@
+import { useSale } from './UserContext';
 import { Card } from './ui/card';
 
 function Date() {
+  const { inicio, final, setInicio, setFinal } = useSale();
   return (
     <Card className="rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-80">
       <div className="flex gap-6 pt-4 pb-2 px-4">
@@ -15,10 +17,15 @@ function Date() {
         <input
           type="date"
           className="w-32 bg-projeto-primary1/50 text-projeto-primary3 rounded text-sm pl-2 py-1"
+          value={inicio}
+          onChange={(e) => setInicio(e.target.value)}
         />
+
         <input
           type="date"
           className="w-32 bg-projeto-primary1/50 text-projeto-primary3 rounded text-sm pl-2 py-1"
+          value={final}
+          onChange={(e) => setFinal(e.target.value)}
         />
       </div>
     </Card>
