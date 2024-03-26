7a1872d3034224fa8213e6d66d4c3f4b902e107d
Willyan Costa <willyancr@gmail.com>
2024-03-26 19:38:03 -0300

    feat: Refactor TriCardStatus component to handle loading state
    Add conditional rendering to display "Carregando..." when loading data in TriCardStatus component. This improves user experience by providing feedback during data retrieval.

--- src/components/TriCardStatus.tsx
+++ src/components/TriCardStatus.tsx
@@ -1,37 +1,54 @@
 import { useSale } from '../components/UserContext';
 
 function TriCardStatus() {
-  const { data, calculateTotalSales, calculateReceived, calculateProcessing } =
-    useSale();
+  const {
+    data,
+    calculateTotalSales,
+    calculateReceived,
+    calculateProcessing,
+    loading,
+  } = useSale();
 
   return (
     <div className="flex justify-between gap-2 text-projeto-primary3">
       <div className="flex flex-col gap-4 font-bold rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-52 pl-4 py-2">
         <p>Vendas</p>
-        <p>
-          {calculateTotalSales(data).toLocaleString('pt-BR', {
-            style: 'currency',
-            currency: 'BRL',
-          })}
-        </p>
+        {loading ? (
+          <div>Carregando...</div>
+        ) : (
+          <p>
+            {calculateTotalSales(data).toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            })}
+          </p>
+        )}
       </div>
       <div className="flex flex-col gap-4 font-bold rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-52 pl-4 py-2">
         <p>Recebido</p>
-        <p>
-          {calculateReceived(data).toLocaleString('pt-BR', {
-            style: 'currency',
-            currency: 'BRL',
-          })}
-        </p>
+        {loading ? (
+          <div>Carregando...</div>
+        ) : (
+          <p>
+            {calculateReceived(data).toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            })}
+          </p>
+        )}
       </div>
       <div className="flex flex-col gap-4 font-bold rounded-[6px] bg-projeto-primary1/10 border-[1px] border-projeto-primary1/20 w-52 pl-4 py-2">
         <p>Processando</p>
-        <p>
-          {calculateProcessing(data).toLocaleString('pt-BR', {
-            style: 'currency',
-            currency: 'BRL',
-          })}
-        </p>
+        {loading ? (
+          <div>Carregando...</div>
+        ) : (
+          <p>
+            {calculateProcessing(data).toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            })}
+          </p>
+        )}
       </div>
     </div>
   );
