767e979d5c323107c074ec141e6e34092947b63e
Willyan Costa <willyancr@gmail.com>
2024-03-25 22:21:44 -0300

    feat: Remove UserContextProvider from App component
    The UserContextProvider was removed from the App component as it is no longer needed for rendering the Home component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import './App.css';
 import Home from './components/Home';
-import UserContextProvider from './components/UserContext';
 
 function App() {
   return (
-    <UserContextProvider>
-      <div>
-        <Home />
-      </div>
-    </UserContextProvider>
+    <div>
+      <Home />
+    </div>
   );
 }
 
