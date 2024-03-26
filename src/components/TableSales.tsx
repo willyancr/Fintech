9b47624525e7c04a3b4abe11bf722c74d7cd3891
Willyan Costa <willyancr@gmail.com>
2024-03-26 00:36:20 -0300

    feat: Add SearchSales component to TableSales
    Import SearchSales component and implement search functionality in TableSales component.

--- src/components/TableSales.tsx
+++ src/components/TableSales.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   Table,
   TableBody,
@@ -9,35 +10,43 @@ import {
   TableRow,
 } from '@/components/ui/table';
 import { useNavigate } from 'react-router-dom';
-import { useSale } from './UserContext';
+import { useSale } from '../components/UserContext';
+import SearchSales from './SearchSales';
 
 function TableSales() {
   const navigate = useNavigate();
   const { data } = useSale();
-
+  const [search, setSearch] = React.useState('');
   function handleClick() {
     navigate('/vendas/id');
   }
   return (
-    <Table className="text-projeto-primary3">
-      <TableCaption>Uma lista das suas faturas recentes.</TableCaption>
-      <TableHeader>
-        <TableRow className="bg-projeto-primary1/20">
-          <TableHead>ID</TableHead>
-          <TableHead>Nome</TableHead>
-          <TableHead>Valor</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody onClick={handleClick} className="cursor-pointer text-white">
-        {data?.map((sale) => (
-          <TableRow key={sale.id}>
-            <TableCell>{sale.id}</TableCell>
-            <TableCell>{sale.nome}</TableCell>
-            <TableCell>R$ {sale.preco}</TableCell>
+    <>
+      <SearchSales search={search} setSearch={setSearch} />
+      <Table className="text-projeto-primary3">
+        <TableCaption>Uma lista das suas faturas recentes.</TableCaption>
+        <TableHeader>
+          <TableRow className="bg-projeto-primary1/20">
+            <TableHead>ID</TableHead>
+            <TableHead>Nome</TableHead>
+            <TableHead>Valor</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody onClick={handleClick} className="cursor-pointer text-white">
+          {data
+            ?.filter((sale: { id: string; nome: string; preco: number }) =>
+              sale.nome.toLowerCase().includes(search.toLowerCase()),
+            )
+            .map((sale: { id: string; nome: string; preco: number }) => (
+              <TableRow key={sale.id}>
+                <TableCell>{sale.id}</TableCell>
+                <TableCell>{sale.nome}</TableCell>
+                <TableCell>R$ {sale.preco}</TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </>
   );
 }
 
