5c49806380fe93c19497907615c29d1d6722558d
Willyan Costa <willyancr@gmail.com>
2024-03-27 16:19:12 -0300

    feat: Refactor GraphicResume component for better data visualization
    Extract data transformation logic into a separate function for improved readability
    and maintainability. Add recharts components for displaying sales data graphically.

--- src/components/GraphicResume.tsx
+++ src/components/GraphicResume.tsx
@@ -1,9 +1,74 @@
-import React from 'react';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
+import { Sale, useSale } from './UserContext';
+
+type SaleDay = {
+  data: string;
+  pago: number;
+  processando: number;
+  falha: number;
+  [key: string]: any;
+};
+// Função que transforma um array de vendas em um array de vendas por dia
+function transformData(data: Sale[]): SaleDay[] {
+  const days: { [key: string]: SaleDay } = {};
+
+  data.forEach((item) => {
+    const day = item.data.split(' ')[0];
+    if (!days[day]) {
+      days[day] = {
+        data: day.substring(5),
+        pago: 0,
+        processando: 0,
+        falha: 0,
+      };
+    }
+    days[day][item.status] += item.preco;
+  });
+
+  return Object.values(days);
+}
 
 function GraphicResume() {
+  const { data } = useSale();
+  const dataGraph = data ? transformData(data) : [];
+
   return (
     <div className="text-projeto-primary3 bg-projeto-primary1/10 h-[600px] p-10 rounded-[6px] border-[1px] border-projeto-primary1/20">
-      GraphicResume
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart data={dataGraph}>
+          <XAxis dataKey="data" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="pago"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+            strokeWidth={2}
+          />
+          <Line
+            type="monotone"
+            dataKey="processando"
+            stroke="#82ca9d"
+            strokeWidth={2}
+          />
+          <Line
+            type="monotone"
+            dataKey="falha"
+            stroke="#ffc658"
+            strokeWidth={2}
+          />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 }
